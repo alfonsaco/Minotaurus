272c000c6f93ce3aaaa7e8ce9633af381484ce09
alfonsaco <alfonso.rincondev@gmail.com>
2025-01-23 23:53:35 +0100

    Se posicionan los jugadores en sus respectivas posiciones

--- Script/juego.js
+++ Script/juego.js
@@ -2,19 +2,11 @@ window.addEventListener("DOMContentLoaded", function() {
     const textoTurno=document.querySelector(".turno");
     let turno=Math.round(Math.random()*1);
 
-    /**
-     *              TURNO
-     *   Par     Turno del jugador Rojo
-     *   Impar   Turno del jugador Azul
-     * 
-     */
-    function turnoJugador(turno) {
-        if(turno%2 == 0) {
-            textoTurno.textContent='TURNO DEL JUGADOR ROJO';
-        } else if(turno%2 != 0) {
-            textoTurno.textContent='TURNO DEL JUGADOR AZUL';
-        }
-    }
+
+
+    // Se posicionan las fichasa en la esquina superior izquierda, e inferior derecha
+    let posicionRojo={x: 1, y: 1};
+    let posicionAzul={x: 19, y: 19};
 
     // Tablero 21x21
     const tablero=[
@@ -40,10 +32,49 @@ window.addEventListener("DOMContentLoaded", function() {
         [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
         [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
     ];
-    
-    function crearTablero(tablero) {
-        const divTablero=document.getElementById("tablero");
 
+    const divTablero=document.getElementById("tablero");
+    const jugRojo=document.getElementById("jugRojo");
+    const jugAzul=document.getElementById("jugAzul");
+    
+
+    // Se define el turno al comenzar y se crea el tablero
+    turnoJugador(turno);
+    crearTablero(tablero);
+
+    const dado=document.querySelector(".dado");
+    // Añadir animación aleatoria al dado
+    dado.addEventListener("click", function() {
+        turno++;
+        turnoJugador(turno);
+        let random=(Math.round(1+Math.random()*5));
+
+        for (let i=1; i<=6; i++) {
+            dado.classList.remove(`dado-${i}`);
+        }
+        void dado.offsetWidth;
+
+        dado.classList.add(`dado-${random}`);
+    });
+
+
+
+
+    /**
+                 TURNO
+        Par     Turno del jugador Rojo
+        Impar   Turno del jugador Azul
+    
+    */
+    function turnoJugador(turno) {
+        if (turno % 2 == 0) {
+            textoTurno.textContent = 'TURNO DEL JUGADOR ROJO';
+        } else if (turno % 2 != 0) {
+            textoTurno.textContent = 'TURNO DEL JUGADOR AZUL';
+        }
+    }
+
+    function crearTablero(tablero) {
         tablero.forEach((fila, y) => {
             fila.forEach((celda, x) => {
                 const divCelda=document.createElement("div");
@@ -65,35 +96,33 @@ window.addEventListener("DOMContentLoaded", function() {
         });
 
         // Posición de los jugadores
-        const jugRojo=document.getElementById("jugRojo");
-        const jugAzul=document.getElementById("jugAzul");
-
-        let anchuraTablero=tablero.offsetWidth;
-        let alturaTablero=tablero.offsetHeight;
-
-        console.log(anchuraTablero);
-        console.log(alturaTablero);
-
-        jugAzul.style.transform=`translate()`;
-        jugRojo.style.transform=`translate()`;
+        posicionarJugadores();
     }
 
-    // Se define el turno al comenzar y se crea el tablero
-    turnoJugador(turno);
-    crearTablero(tablero);
+    function obtenerTamañoCelda() {
+        const celda=divTablero.querySelector(".celda");
+        // Esto se usa para alto y ancho, ya que ocupa lo mismo
+        const anchoCelda=celda.offsetWidth;
+        const altoCelda=celda.offsetHeight;
 
-    const dado=document.querySelector(".dado");
-    // Añadir animación aleatoria al dado
-    dado.addEventListener("click", function() {
-        turno++;
-        turnoJugador(turno);
-        let random=(Math.round(1+Math.random()*5));
+        return {ancho: anchoCelda, alto: altoCelda};
+    }
 
-        for (let i=1; i<=6; i++) {
-            dado.classList.remove(`dado-${i}`);
-        }
-        void dado.offsetWidth;
+    function posicionarJugadores() {
+        const tamañoCelda=obtenerTamañoCelda();
+        
+        // Posicionar el jugador azul
+        jugAzul.style.left=`${posicionAzul.x * tamañoCelda.ancho}px`;
+        jugAzul.style.top=`${posicionAzul.y * tamañoCelda.alto}px`;
 
-        dado.classList.add(`dado-${random}`);
-    });
+        // Posicionar el jugador rojo
+        jugRojo.style.left=`${posicionRojo.x * tamañoCelda.ancho}px`;
+        jugRojo.style.top=`${posicionRojo.y * tamañoCelda.alto}px`;
+
+        // Cambiamos el tamaño de los jugadores, para que tengan el mismo tamaño que las celdas
+        jugAzul.style.width=`${tamañoCelda.ancho}px`;
+        jugAzul.style.height=`${tamañoCelda.alto}px`;
+        jugRojo.style.width=`${tamañoCelda.ancho}px`;
+        jugRojo.style.height=`${tamañoCelda.alto}px`;
+    }
 });
